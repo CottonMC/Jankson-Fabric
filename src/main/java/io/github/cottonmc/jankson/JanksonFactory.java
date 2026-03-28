a5a28fd8fa24e8b3439d10c8907e61c7d07a0852
Juuz <6596629+Juuxel@users.noreply.github.com>
2026-03-28 11:00:06 +0200

    Add .editorconfig and reformat code

--- src/main/java/io/github/cottonmc/jankson/JanksonFactory.java
+++ src/main/java/io/github/cottonmc/jankson/JanksonFactory.java
@@ -90,17 +90,17 @@ public class JanksonFactory {
 		//Note: specifically excludes dynamic registries since we can't have static access to them.
 		register(builder, Activity.class,                    BuiltInRegistries.ACTIVITY);
 		register(builder, ArgumentTypeInfo.class,            BuiltInRegistries.COMMAND_ARGUMENT_TYPE);
-        register(builder, Attribute.class,                   BuiltInRegistries.ATTRIBUTE);
-        register(builder, AttributeType.class,               BuiltInRegistries.ATTRIBUTE_TYPE);
+		register(builder, Attribute.class,                   BuiltInRegistries.ATTRIBUTE);
+		register(builder, AttributeType.class,               BuiltInRegistries.ATTRIBUTE_TYPE);
 		register(builder, Block.class,                       BuiltInRegistries.BLOCK);
 		register(builder, BlockEntityType.class,             BuiltInRegistries.BLOCK_ENTITY_TYPE);
 		register(builder, BlockPredicateType.class,          BuiltInRegistries.BLOCK_PREDICATE_TYPE);
 		register(builder, BlockStateProviderType.class,      BuiltInRegistries.BLOCKSTATE_PROVIDER_TYPE);
 		register(builder, ChunkStatus.class,                 BuiltInRegistries.CHUNK_STATUS);
-        register(builder, ConsumeEffect.Type.class,          BuiltInRegistries.CONSUME_EFFECT_TYPE);
-        register(builder, CreativeModeTab.class,             BuiltInRegistries.CREATIVE_MODE_TAB);
+		register(builder, ConsumeEffect.Type.class,          BuiltInRegistries.CONSUME_EFFECT_TYPE);
+		register(builder, CreativeModeTab.class,             BuiltInRegistries.CREATIVE_MODE_TAB);
 		register(builder, CriterionTrigger.class,            BuiltInRegistries.TRIGGER_TYPES);
-        register(builder, DataComponentPredicate.Type.class, BuiltInRegistries.DATA_COMPONENT_PREDICATE_TYPE);
+		register(builder, DataComponentPredicate.Type.class, BuiltInRegistries.DATA_COMPONENT_PREDICATE_TYPE);
 		register(builder, DebugSubscription.class,           BuiltInRegistries.DEBUG_SUBSCRIPTION);
 		register(builder, EntityType.class,                  BuiltInRegistries.ENTITY_TYPE);
 		register(builder, EnvironmentAttribute.class,        BuiltInRegistries.ENVIRONMENT_ATTRIBUTE);
@@ -115,8 +115,8 @@ public class JanksonFactory {
 		register(builder, Item.class,                        BuiltInRegistries.ITEM);
 		register(builder, MapDecorationType.class,           BuiltInRegistries.MAP_DECORATION_TYPE);
 		register(builder, MemoryModuleType.class,            BuiltInRegistries.MEMORY_MODULE_TYPE);
-        register(builder, MenuType.class,                    BuiltInRegistries.MENU);
-        register(builder, MobEffect.class,                   BuiltInRegistries.MOB_EFFECT);
+		register(builder, MenuType.class,                    BuiltInRegistries.MENU);
+		register(builder, MobEffect.class,                   BuiltInRegistries.MOB_EFFECT);
 		register(builder, NumberFormatType.class,            BuiltInRegistries.NUMBER_FORMAT_TYPE);
 		register(builder, OutgoingRpcMethod.class,           BuiltInRegistries.OUTGOING_RPC_METHOD);
 		register(builder, ParticleType.class,                BuiltInRegistries.PARTICLE_TYPE);
@@ -141,12 +141,12 @@ public class JanksonFactory {
 		register(builder, StructurePoolElementType.class,    BuiltInRegistries.STRUCTURE_POOL_ELEMENT);
 		register(builder, StructureProcessorType.class,      BuiltInRegistries.STRUCTURE_PROCESSOR);
 		register(builder, StructureType.class,               BuiltInRegistries.STRUCTURE_TYPE);
-        register(builder, TicketType.class,                  BuiltInRegistries.TICKET_TYPE);
+		register(builder, TicketType.class,                  BuiltInRegistries.TICKET_TYPE);
 		register(builder, TreeDecoratorType.class,           BuiltInRegistries.TREE_DECORATOR_TYPE);
 		register(builder, TrunkPlacerType.class,             BuiltInRegistries.TRUNK_PLACER_TYPE);
 		register(builder, VillagerProfession.class,          BuiltInRegistries.VILLAGER_PROFESSION);
 		register(builder, VillagerType.class,                BuiltInRegistries.VILLAGER_TYPE);
-        register(builder, WorldCarver.class,                 BuiltInRegistries.CARVER);
+		register(builder, WorldCarver.class,                 BuiltInRegistries.CARVER);
 		register(builder, Registry.class,                           BuiltInRegistries.REGISTRY);
 		
 		return builder;
@@ -163,7 +163,7 @@ public class JanksonFactory {
 	
 	private static <T, U extends T> JsonElement lookupSerialize(T t, Registry<U> registry) {
 		@SuppressWarnings("unchecked") //Widening cast happening because of generic type parameters in the registry class
-        Identifier id = registry.getKey((U)t);
+		Identifier id = registry.getKey((U)t);
 		if (id==null) return JsonNull.INSTANCE;
 		return new JsonPrimitive(id.toString());
 	}
@@ -173,37 +173,37 @@ public class JanksonFactory {
 		return builder().build();
 	}
 
-    /**
-     * Registers a codec-based serializer and deserializer to a Jankson builder.
-     *
-     * @param builder the builder
-     * @param type    the serialized type
-     * @param codec   the codec
-     * @param <T>     the serialized type
-     * @since 12.0.0
-     */
-    public static <T> void registerCodecBasedSerializer(Jankson.Builder builder, Class<T> type, Codec<T> codec) {
-        registerCodecBasedSerializer(builder, type, codec, JanksonOps.INSTANCE);
-    }
+	/**
+	 * Registers a codec-based serializer and deserializer to a Jankson builder.
+	 *
+	 * @param builder the builder
+	 * @param type    the serialized type
+	 * @param codec   the codec
+	 * @param <T>     the serialized type
+	 * @since 12.0.0
+	 */
+	public static <T> void registerCodecBasedSerializer(Jankson.Builder builder, Class<T> type, Codec<T> codec) {
+		registerCodecBasedSerializer(builder, type, codec, JanksonOps.INSTANCE);
+	}
 
-    /**
-     * Registers a codec-based serializer and deserializer to a Jankson builder.
-     * This overload can use a different {@link DynamicOps} instance (e.g. {@link net.minecraft.resources.RegistryOps}).
-     *
-     * @param builder the builder
-     * @param type    the serialized type
-     * @param codec   the codec
-     * @param ops     the {@link DynamicOps}
-     * @param <T>     the serialized type
-     * @since 12.0.0
-     */
-    public static <T> void registerCodecBasedSerializer(Jankson.Builder builder, Class<T> type, Codec<T> codec, DynamicOps<JsonElement> ops) {
-        builder.registerDeserializer(JsonElement.class, type, (jsonElement, m) -> {
-            return codec.parse(ops, jsonElement).getOrThrow(DeserializationException::new);
-        });
+	/**
+	 * Registers a codec-based serializer and deserializer to a Jankson builder.
+	 * This overload can use a different {@link DynamicOps} instance (e.g. {@link net.minecraft.resources.RegistryOps}).
+	 *
+	 * @param builder the builder
+	 * @param type    the serialized type
+	 * @param codec   the codec
+	 * @param ops     the {@link DynamicOps}
+	 * @param <T>     the serialized type
+	 * @since 12.0.0
+	 */
+	public static <T> void registerCodecBasedSerializer(Jankson.Builder builder, Class<T> type, Codec<T> codec, DynamicOps<JsonElement> ops) {
+		builder.registerDeserializer(JsonElement.class, type, (jsonElement, m) -> {
+			return codec.parse(ops, jsonElement).getOrThrow(DeserializationException::new);
+		});
 
-        builder.registerSerializer(type, (t, marshaller) -> {
-            return codec.encodeStart(ops, t).getOrThrow();
-        });
-    }
+		builder.registerSerializer(type, (t, marshaller) -> {
+			return codec.encodeStart(ops, t).getOrThrow();
+		});
+	}
 }
--- src/main/java/io/github/cottonmc/jankson/JanksonOps.java
+++ src/main/java/io/github/cottonmc/jankson/JanksonOps.java
@@ -21,290 +21,290 @@ import java.util.stream.Stream;
  * Some methods are based on Mojang's JsonOps for Gson.
  */
 public class JanksonOps implements DynamicOps<JsonElement> {
-    public static final JanksonOps INSTANCE = new JanksonOps(false);
-    public static final JanksonOps COMPRESSED = new JanksonOps(true);
+	public static final JanksonOps INSTANCE = new JanksonOps(false);
+	public static final JanksonOps COMPRESSED = new JanksonOps(true);
 
-    private final boolean compressed;
+	private final boolean compressed;
 
-    /**
-     * Constructs a JanksonOps instance.
-     *
-     * @param compressed true if strings should be treated as valid numbers,
-     *                   other primitives should be treated as valid strings,
-     *                   and maps {@linkplain DynamicOps#compressMaps() should be compressed}.
-     */
-    protected JanksonOps(boolean compressed) {
-        this.compressed = compressed;
-    }
+	/**
+	 * Constructs a JanksonOps instance.
+	 *
+	 * @param compressed true if strings should be treated as valid numbers,
+	 *                   other primitives should be treated as valid strings,
+	 *                   and maps {@linkplain DynamicOps#compressMaps() should be compressed}.
+	 */
+	protected JanksonOps(boolean compressed) {
+		this.compressed = compressed;
+	}
 
-    @Override
-    public JsonElement empty() {
-        return JsonNull.INSTANCE;
-    }
+	@Override
+	public JsonElement empty() {
+		return JsonNull.INSTANCE;
+	}
 
-    @Override
-    public <U> U convertTo(DynamicOps<U> outOps, JsonElement input) {
-        if (input instanceof JsonObject) {
-            return convertMap(outOps, input);
-        } else if (input instanceof JsonArray) {
-            return convertList(outOps, input);
-        } else if (input instanceof JsonPrimitive) {
-            Object value = ((JsonPrimitive) input).getValue();
+	@Override
+	public <U> U convertTo(DynamicOps<U> outOps, JsonElement input) {
+		if (input instanceof JsonObject) {
+			return convertMap(outOps, input);
+		} else if (input instanceof JsonArray) {
+			return convertList(outOps, input);
+		} else if (input instanceof JsonPrimitive) {
+			Object value = ((JsonPrimitive) input).getValue();
 
-            if (value instanceof Byte) {
-                return outOps.createByte((Byte) value);
-            } else if (value instanceof Short) {
-                return outOps.createShort((Short) value);
-            } else if (value instanceof Integer) {
-                return outOps.createInt((Integer) value);
-            } else if (value instanceof Long) {
-                return outOps.createLong((Long) value);
-            } else if (value instanceof Float) {
-                return outOps.createFloat((Float) value);
-            } else if (value instanceof Double) {
-                return outOps.createDouble((Double) value);
-            } else if (value instanceof Number) {
-                return outOps.createNumeric((Number) value);
-            } else if (value instanceof Boolean) {
-                return outOps.createBoolean((Boolean) value);
-            } else if (value instanceof String) {
-                return outOps.createString((String) value);
-            }
-        }
+			if (value instanceof Byte) {
+				return outOps.createByte((Byte) value);
+			} else if (value instanceof Short) {
+				return outOps.createShort((Short) value);
+			} else if (value instanceof Integer) {
+				return outOps.createInt((Integer) value);
+			} else if (value instanceof Long) {
+				return outOps.createLong((Long) value);
+			} else if (value instanceof Float) {
+				return outOps.createFloat((Float) value);
+			} else if (value instanceof Double) {
+				return outOps.createDouble((Double) value);
+			} else if (value instanceof Number) {
+				return outOps.createNumeric((Number) value);
+			} else if (value instanceof Boolean) {
+				return outOps.createBoolean((Boolean) value);
+			} else if (value instanceof String) {
+				return outOps.createString((String) value);
+			}
+		}
 
-        return outOps.empty();
-    }
+		return outOps.empty();
+	}
 
-    @Override
-    public DataResult<Number> getNumberValue(JsonElement input) {
-        if (input instanceof JsonPrimitive) {
-            Object value = ((JsonPrimitive) input).getValue();
-            if (value instanceof Number) {
-                return DataResult.success((Number) value);
-            } else if (value instanceof Boolean) {
-                return DataResult.success((Boolean) value ? 1 : 0);
-            } else if (compressed && value instanceof String) {
-                // See JsonOps.getNumberValue
-                try {
-                    return DataResult.success(Integer.parseInt((String) value));
-                } catch (final NumberFormatException e) {
-                    return DataResult.error(() -> "Not a number: " + e + " " + input);
-                }
-            }
-        }
+	@Override
+	public DataResult<Number> getNumberValue(JsonElement input) {
+		if (input instanceof JsonPrimitive) {
+			Object value = ((JsonPrimitive) input).getValue();
+			if (value instanceof Number) {
+				return DataResult.success((Number) value);
+			} else if (value instanceof Boolean) {
+				return DataResult.success((Boolean) value ? 1 : 0);
+			} else if (compressed && value instanceof String) {
+				// See JsonOps.getNumberValue
+				try {
+					return DataResult.success(Integer.parseInt((String) value));
+				} catch (final NumberFormatException e) {
+					return DataResult.error(() -> "Not a number: " + e + " " + input);
+				}
+			}
+		}
 
-        return DataResult.error(() -> "Not a number: " + input);
-    }
+		return DataResult.error(() -> "Not a number: " + input);
+	}
 
-    @Override
-    public JsonElement createNumeric(Number i) {
-        return new JsonPrimitive(i);
-    }
+	@Override
+	public JsonElement createNumeric(Number i) {
+		return new JsonPrimitive(i);
+	}
 
-    @Override
-    public DataResult<String> getStringValue(JsonElement input) {
-        if (input instanceof JsonPrimitive) {
-            JsonPrimitive primitive = (JsonPrimitive) input;
-            if (compressed || primitive.getValue() instanceof String) {
-                return DataResult.success(primitive.asString());
-            }
-        }
+	@Override
+	public DataResult<String> getStringValue(JsonElement input) {
+		if (input instanceof JsonPrimitive) {
+			JsonPrimitive primitive = (JsonPrimitive) input;
+			if (compressed || primitive.getValue() instanceof String) {
+				return DataResult.success(primitive.asString());
+			}
+		}
 
-        return DataResult.error(() -> "Not a string: " + input);
-    }
+		return DataResult.error(() -> "Not a string: " + input);
+	}
 
-    @Override
-    public JsonElement createString(String value) {
-        return new JsonPrimitive(value);
-    }
+	@Override
+	public JsonElement createString(String value) {
+		return new JsonPrimitive(value);
+	}
 
-    @Override
-    public JsonElement createBoolean(boolean value) {
-        return value ? JsonPrimitive.TRUE : JsonPrimitive.FALSE;
-    }
+	@Override
+	public JsonElement createBoolean(boolean value) {
+		return value ? JsonPrimitive.TRUE : JsonPrimitive.FALSE;
+	}
 
-    @Override
-    public DataResult<JsonElement> mergeToList(JsonElement list, JsonElement value) {
-        if (list instanceof JsonNull) {
-            JsonArray output = new JsonArray();
-            output.add(value);
-            return DataResult.success(output);
-        } else if (list instanceof JsonArray) {
-            JsonArray output = new JsonArray();
-            output.addAll((JsonArray) list);
-            output.add(value);
-            return DataResult.success(output);
-        }
+	@Override
+	public DataResult<JsonElement> mergeToList(JsonElement list, JsonElement value) {
+		if (list instanceof JsonNull) {
+			JsonArray output = new JsonArray();
+			output.add(value);
+			return DataResult.success(output);
+		} else if (list instanceof JsonArray) {
+			JsonArray output = new JsonArray();
+			output.addAll((JsonArray) list);
+			output.add(value);
+			return DataResult.success(output);
+		}
 
-        return DataResult.error(() -> "Not an array: " + list);
-    }
+		return DataResult.error(() -> "Not an array: " + list);
+	}
 
-    @Override
-    public DataResult<JsonElement> mergeToMap(JsonElement map, JsonElement key, JsonElement value) {
-        if (!(key instanceof JsonPrimitive) || (!(((JsonPrimitive) key).getValue() instanceof String) && !compressed)) {
-            return DataResult.error(() -> "Key is not a string: " + key);
-        }
+	@Override
+	public DataResult<JsonElement> mergeToMap(JsonElement map, JsonElement key, JsonElement value) {
+		if (!(key instanceof JsonPrimitive) || (!(((JsonPrimitive) key).getValue() instanceof String) && !compressed)) {
+			return DataResult.error(() -> "Key is not a string: " + key);
+		}
 
-        if (map instanceof JsonNull) {
-            JsonObject output = new JsonObject();
-            output.put(((JsonPrimitive) key).asString(), value);
-            return DataResult.success(output);
-        } else if (map instanceof JsonObject) {
-            JsonObject output = new JsonObject();
-            output.put(((JsonPrimitive) key).asString(), value);
-            output.putAll((JsonObject) map);
-            return DataResult.success(output);
-        } else {
-            return DataResult.error(() -> "Not a JSON object: " + map);
-        }
-    }
+		if (map instanceof JsonNull) {
+			JsonObject output = new JsonObject();
+			output.put(((JsonPrimitive) key).asString(), value);
+			return DataResult.success(output);
+		} else if (map instanceof JsonObject) {
+			JsonObject output = new JsonObject();
+			output.put(((JsonPrimitive) key).asString(), value);
+			output.putAll((JsonObject) map);
+			return DataResult.success(output);
+		} else {
+			return DataResult.error(() -> "Not a JSON object: " + map);
+		}
+	}
 
-    @Override
-    public DataResult<JsonElement> mergeToMap(JsonElement map, MapLike<JsonElement> values) {
-        if (!(map instanceof JsonObject) && !(map instanceof JsonNull)) {
-            return DataResult.error(() -> "Not a JSON object: " + map);
-        }
+	@Override
+	public DataResult<JsonElement> mergeToMap(JsonElement map, MapLike<JsonElement> values) {
+		if (!(map instanceof JsonObject) && !(map instanceof JsonNull)) {
+			return DataResult.error(() -> "Not a JSON object: " + map);
+		}
 
-        JsonObject result = new JsonObject();
-        if (map instanceof JsonObject) {
-            result.putAll((JsonObject) map);
-        }
+		JsonObject result = new JsonObject();
+		if (map instanceof JsonObject) {
+			result.putAll((JsonObject) map);
+		}
 
-        List<JsonElement> invalidKeys = new ArrayList<>();
+		List<JsonElement> invalidKeys = new ArrayList<>();
 
-        values.entries().forEach(entry -> {
-            JsonElement key = entry.getFirst();
+		values.entries().forEach(entry -> {
+			JsonElement key = entry.getFirst();
 
-            if (!(key instanceof JsonPrimitive) || (!(((JsonPrimitive) key).getValue() instanceof String && !compressed))) {
-                invalidKeys.add(key);
-                return;
-            }
+			if (!(key instanceof JsonPrimitive) || (!(((JsonPrimitive) key).getValue() instanceof String && !compressed))) {
+				invalidKeys.add(key);
+				return;
+			}
 
-            result.put(((JsonPrimitive) key).asString(), entry.getSecond());
-        });
+			result.put(((JsonPrimitive) key).asString(), entry.getSecond());
+		});
 
-        if (!invalidKeys.isEmpty()) {
-            return DataResult.error(() -> "Some keys are not strings: " + invalidKeys, result);
-        }
+		if (!invalidKeys.isEmpty()) {
+			return DataResult.error(() -> "Some keys are not strings: " + invalidKeys, result);
+		}
 
-        return DataResult.success(result);
-    }
+		return DataResult.success(result);
+	}
 
-    @Override
-    public DataResult<Stream<Pair<JsonElement, JsonElement>>> getMapValues(JsonElement input) {
-        if (input instanceof JsonObject) {
-            return DataResult.success(((JsonObject) input).entrySet().stream()
-                    .map(entry -> new Pair<>(new JsonPrimitive(entry.getKey()), entry.getValue())));
-        }
-        return DataResult.error(() -> "Not a JSON object: " + input);
-    }
+	@Override
+	public DataResult<Stream<Pair<JsonElement, JsonElement>>> getMapValues(JsonElement input) {
+		if (input instanceof JsonObject) {
+			return DataResult.success(((JsonObject) input).entrySet().stream()
+				.map(entry -> new Pair<>(new JsonPrimitive(entry.getKey()), entry.getValue())));
+		}
+		return DataResult.error(() -> "Not a JSON object: " + input);
+	}
 
-    @Override
-    public JsonElement createMap(Map<JsonElement, JsonElement> map) {
-        JsonObject result = new JsonObject();
-        for (Map.Entry<JsonElement, JsonElement> entry : map.entrySet()) {
-            result.put(((JsonPrimitive) entry.getKey()).asString(), entry.getValue());
-        }
-        return result;
-    }
+	@Override
+	public JsonElement createMap(Map<JsonElement, JsonElement> map) {
+		JsonObject result = new JsonObject();
+		for (Map.Entry<JsonElement, JsonElement> entry : map.entrySet()) {
+			result.put(((JsonPrimitive) entry.getKey()).asString(), entry.getValue());
+		}
+		return result;
+	}
 
-    @Override
-    public JsonElement createMap(Stream<Pair<JsonElement, JsonElement>> map) {
-        JsonObject result = new JsonObject();
-        map.forEach(pair -> result.put(((JsonPrimitive) pair.getFirst()).asString(), pair.getSecond()));
-        return result;
-    }
+	@Override
+	public JsonElement createMap(Stream<Pair<JsonElement, JsonElement>> map) {
+		JsonObject result = new JsonObject();
+		map.forEach(pair -> result.put(((JsonPrimitive) pair.getFirst()).asString(), pair.getSecond()));
+		return result;
+	}
 
-    @Override
-    public DataResult<MapLike<JsonElement>> getMap(JsonElement input) {
-        if (!(input instanceof JsonObject)) {
-            return DataResult.error(() -> "Not a JSON object: " + input);
-        }
+	@Override
+	public DataResult<MapLike<JsonElement>> getMap(JsonElement input) {
+		if (!(input instanceof JsonObject)) {
+			return DataResult.error(() -> "Not a JSON object: " + input);
+		}
 
-        JsonObject obj = (JsonObject) input;
-        return DataResult.success(new MapLike<JsonElement>() {
-            @Override
-            public JsonElement get(JsonElement key) {
-                return obj.get(((JsonPrimitive) key).asString());
-            }
+		JsonObject obj = (JsonObject) input;
+		return DataResult.success(new MapLike<JsonElement>() {
+			@Override
+			public JsonElement get(JsonElement key) {
+				return obj.get(((JsonPrimitive) key).asString());
+			}
 
-            @Override
-            public JsonElement get(String key) {
-                return obj.get(key);
-            }
+			@Override
+			public JsonElement get(String key) {
+				return obj.get(key);
+			}
 
-            @Override
-            public Stream<Pair<JsonElement, JsonElement>> entries() {
-                return obj.entrySet().stream()
-                        .map(entry -> new Pair<>(new JsonPrimitive(entry.getKey()), entry.getValue()));
-            }
+			@Override
+			public Stream<Pair<JsonElement, JsonElement>> entries() {
+				return obj.entrySet().stream()
+					.map(entry -> new Pair<>(new JsonPrimitive(entry.getKey()), entry.getValue()));
+			}
 
-            @Override
-            public String toString() {
-                return "MapLike[" + obj + "]";
-            }
-        });
-    }
+			@Override
+			public String toString() {
+				return "MapLike[" + obj + "]";
+			}
+		});
+	}
 
-    @Override
-    public DataResult<Stream<JsonElement>> getStream(JsonElement input) {
-        if (input instanceof JsonArray) {
-            return DataResult.success(((JsonArray) input).stream());
-        }
-        return DataResult.error(() -> "Not an array: " + input);
-    }
+	@Override
+	public DataResult<Stream<JsonElement>> getStream(JsonElement input) {
+		if (input instanceof JsonArray) {
+			return DataResult.success(((JsonArray) input).stream());
+		}
+		return DataResult.error(() -> "Not an array: " + input);
+	}
 
-    @Override
-    public JsonElement createList(Stream<JsonElement> input) {
-        JsonArray result = new JsonArray();
-        input.forEach(result::add);
-        return result;
-    }
+	@Override
+	public JsonElement createList(Stream<JsonElement> input) {
+		JsonArray result = new JsonArray();
+		input.forEach(result::add);
+		return result;
+	}
 
-    @Override
-    public DataResult<Consumer<Consumer<JsonElement>>> getList(JsonElement input) {
-        if (!(input instanceof JsonArray)) {
-            return DataResult.error(() -> "Not an array: " + input);
-        }
+	@Override
+	public DataResult<Consumer<Consumer<JsonElement>>> getList(JsonElement input) {
+		if (!(input instanceof JsonArray)) {
+			return DataResult.error(() -> "Not an array: " + input);
+		}
 
-        return DataResult.success(sink -> {
-            for (JsonElement element : (JsonArray) input) {
-                sink.accept(element);
-            }
-        });
-    }
+		return DataResult.success(sink -> {
+			for (JsonElement element : (JsonArray) input) {
+				sink.accept(element);
+			}
+		});
+	}
 
-    @Override
-    public boolean compressMaps() {
-        return compressed;
-    }
+	@Override
+	public boolean compressMaps() {
+		return compressed;
+	}
 
-    @Override
-    public JsonElement emptyList() {
-        return new JsonArray();
-    }
+	@Override
+	public JsonElement emptyList() {
+		return new JsonArray();
+	}
 
-    @Override
-    public JsonElement emptyMap() {
-        return new JsonObject();
-    }
+	@Override
+	public JsonElement emptyMap() {
+		return new JsonObject();
+	}
 
-    @Override
-    public JsonElement remove(JsonElement input, String key) {
-        if (input instanceof JsonObject) {
-            JsonObject result = new JsonObject();
-            for (Map.Entry<String, JsonElement> entry : ((JsonObject) input).entrySet()) {
-                if (!entry.getKey().equals(key)) {
-                    result.put(entry.getKey(), entry.getValue());
-                }
-            }
-            return result;
-        }
-        return input;
-    }
+	@Override
+	public JsonElement remove(JsonElement input, String key) {
+		if (input instanceof JsonObject) {
+			JsonObject result = new JsonObject();
+			for (Map.Entry<String, JsonElement> entry : ((JsonObject) input).entrySet()) {
+				if (!entry.getKey().equals(key)) {
+					result.put(entry.getKey(), entry.getValue());
+				}
+			}
+			return result;
+		}
+		return input;
+	}
 
-    @Override
-    public String toString() {
-        return "Jankson";
-    }
+	@Override
+	public String toString() {
+		return "Jankson";
+	}
 }
